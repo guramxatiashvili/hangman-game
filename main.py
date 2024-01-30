48c36c5a9515c7762f640973106cf851b9aacb2f
guramxatiashvili <guramxatiashvili@hotmail.com>
2024-01-30 14:18:28 +0000

    assign correct user guess and display it

--- main.py
+++ main.py
@@ -4,18 +4,22 @@ import random
 word_list = ["aardvark", "baboon", "camel"]
 
 chosen_word=random.choice(word_list)
-#print(chosen_word)
-user_guess=input("Guess a letter:").lower()
 
+display=[]
+for letter in chosen_word:
+    display+= "_"
+
+print(display)
+user_guess=input("Guess a letter:").lower()
+#assign correct user guess and display it
+for position in range(len(chosen_word)):
+    letter=chosen_word[position]
+    if letter==user_guess:
+        display[position]=letter
+print(display)
+#check if user guess letter is in the chosen word
 for letter in chosen_word:
     if user_guess == letter:
         print("Right")
     else:
-        print("Wrong")
-
-
-#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
-
-#TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-
-#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+        print("Wrong") 
