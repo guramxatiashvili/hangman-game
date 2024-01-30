7aea99243d7680da5634ac5a5d5c5d187557d4fc
guramxatiashvili <guramxatiashvili@hotmail.com>
2024-01-30 15:13:10 +0000

    win game logic

--- main.py
+++ main.py
@@ -4,22 +4,31 @@ import random
 word_list = ["aardvark", "baboon", "camel"]
 
 chosen_word=random.choice(word_list)
-
+print(f'chose word is : {chosen_word}')
 display=[]
+
+word_length=len(chosen_word)
 for letter in chosen_word:
     display+= "_"
 
 print(display)
-user_guess=input("Guess a letter:").lower()
-#assign correct user guess and display it
-for position in range(len(chosen_word)):
-    letter=chosen_word[position]
-    if letter==user_guess:
-        display[position]=letter
-print(display)
-#check if user guess letter is in the chosen word
-for letter in chosen_word:
-    if user_guess == letter:
-        print("Right")
-    else:
-        print("Wrong") 
+
+end_of_game=False
+while not end_of_game:
+    user_guess=input("Guess a letter:").lower()
+    #assign correct user guess and display it
+    for position in range(word_length):
+        letter=chosen_word[position] 
+        if letter==user_guess:
+            display[position]=letter
+    #if no '_' in display, then end of game        
+    if '_' not in display:
+        end_of_game=True
+        print("You win!")
+    print(display)
+
+# for letter in chosen_word:
+#     if user_guess == letter:
+#         print("Right")
+#     else:
+#         print("Wrong") 
