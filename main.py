6247b6ae80ca76c157ea51869695729f656cf930
guramxatiashvili <guramxatiashvili@hotmail.com>
2024-01-30 15:59:55 +0000

    add ascii character position based on the remaining lives

--- main.py
+++ main.py
@@ -1,20 +1,78 @@
 #Step 1 
 import random
 
-word_list = ["aardvark", "baboon", "camel"]
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
 
+word_list = ["aardvark", "baboon", "camel"]
 chosen_word=random.choice(word_list)
+
 print(f'chose word is : {chosen_word}')
 display=[]
 word_length=len(chosen_word)
 lives=6
+end_of_game=False
+
 
 for letter in chosen_word:
     display+= "_"
 
 print(display)
 
-end_of_game=False
 while not end_of_game:
     user_guess=input("Guess a letter:").lower()
     #assign correct user guess and display it
@@ -35,3 +93,4 @@ while not end_of_game:
         end_of_game=True
         print("You win!")
     print(display)
+    print(stages[lives]) #show ascii character position based on remaning lives
